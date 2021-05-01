c78cacfda68bafe4aae4d31317f5bfd79ac713ab
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-05-01 11:30:26 +0200

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from '../assets/img/logo-white.png'
+import "../styles/Footer.css";
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="main-footer">
+                <img src={logo} alt="Kasa" />
+                <p>
+                    <span className="far fa-copyright"></span> 2020 Kasa. All rights
+                    reserved
+                </p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
