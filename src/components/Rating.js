518a3bf33638438ae901ad57e48076b319bf03e7
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-05-02 16:15:50 +0200

    rating

---
+++ src/components/Rating.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../css/Rating.css";
+import full_star from '../assets/svg/red-star-solid.svg'
+import light_star from '../assets/svg/light-star-solid.svg'
+
+class Rating extends React.Component {
+    render() {
+        const { fill } = this.props;
+        const starsClasses = [];
+
+        for (let i = 0; i < 5; i++) {
+            i < fill
+                ? starsClasses.push({full_star})
+                : starsClasses.push({light_star});
+        }
+
+        return (
+            <div className="rate">
+                {starsClasses.map((star, index) => (
+                    <img key={index} src={star}></img>
+                ))}
+            </div>
+        );
+    }
+}
+
+
+export default Rating;
