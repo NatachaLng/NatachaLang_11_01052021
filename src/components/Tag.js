7f3073f11f630d5104de7b16a98567825f188a6d
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-05-02 16:17:10 +0200

    Tags

---
+++ src/components/Tag.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Tag.css";
+
+class Tag extends React.Component {
+    render() {
+        const { content } = this.props;
+
+        return <div className="tag">{content}</div>;
+    }
+}
+
+
+export default Tag;
