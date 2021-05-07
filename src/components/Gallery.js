880ad6463475c2bc7b3829643d7fbaa3ddcd22cf
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-05-07 08:52:50 +0100

    refactor gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,52 +3,34 @@ import "../styles/Gallery.css";
 import left_arrow from '../assets/svg/chevron-left-solid.svg';
 import right_arrow from '../assets/svg/chevron-right-solid.svg'
 
-class Gallery extends React.Component {  state = {
-    currentPicture: 0,
-};
 
-    goToPreviousImage = () => {
+class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPicture: 0,
+        };
+    }
+
+    handlePreviousClick = () => {
         this.setState((prevState) => ({
-            currentPicture:
-                prevState.currentPicture === 0
-                    ? this.props.images.length - 1
-                    : prevState.currentPicture - 1,
+            currentPicture: prevState.currentPicture === 0 ? this.props.images.length - 1 : prevState.currentPicture - 1,
         }));
     };
-
-    goToNextImage = () => {
+    handleNextClick = () => {
         this.setState((prevState) => ({
-            currentPicture:
-                prevState.currentPicture === this.props.images.length - 1
-                    ? 0
-                    : prevState.currentPicture + 1,
+            currentPicture: prevState.currentPicture === this.props.images.length - 1 ? 0 : prevState.currentPicture + 1,
         }));
     };
-
     render() {
-        const { images } = this.props;
-        const { currentPicture } = this.state;
-        const title = this.props.title
-
         return (
             <div className="gallery">
-                <img class='gallery_image' src={images[currentPicture]} alt={title}/>
-
-                {images.length > 1 && (
-                    <img
-                        className='left-arrow arrow'
-                        src={left_arrow}
-                        alt="Précédent"
-                        onClick={() => this.goToPreviousImage()}
-                    ></img>
-                )}
-                {images.length > 1 && (
-                    <img
-                        className='right-arrow arrow'
-                        src={right_arrow}
-                        alt="Suivant"
-                        onClick={() => this.goToNextImage()}
-                    ></img>
+                <img className="gallery_image" src={this.props.images[this.state.currentPicture]} alt="location" />
+                {this.props.images.length > 1 && (
+                    <>
+                        <img className="left-arrow arrow" src={left_arrow} onClick={this.handlePreviousClick} alt="Previous" />
+                        <img className="right-arrow arrow" src={right_arrow} onClick={this.handleNextClick} alt="Next" />
+                    </>
                 )}
             </div>
         );
